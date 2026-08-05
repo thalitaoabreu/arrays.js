c7572eb55784fa35528b5e703336b8ff8ac9a59b
Thalita Oliveira <thalita.oabreu@outlook.com>
2026-08-05 12:59:18 -0300

    exercicios resolvidos do mod2

--- exercicios/mod2/ex5.js
+++ exercicios/mod2/ex5.js
@@ -12,5 +12,12 @@ for (let linha = 0; linha < 3; linha++) {
   // aqui, fora do for de coluna, mas dentro do for de linha:
   matriz.push(linhaAtual);
 }
+console.log(matriz);
 
+// ex 6:
+console.log(matriz[1][2]); // -> acessando o elemento da linha 1 e coluna 2 = elemento 6
+
+
+// ex 7:
+matriz[2][1] = 15;
 console.log(matriz);

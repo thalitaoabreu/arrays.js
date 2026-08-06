c264c41ddf5cda9a50019faff326be2947538923
Thalita Oliveira <thalita.oabreu@outlook.com>
2026-08-06 12:40:26 -0300

    estudo destructuring

---
+++ conteúdo Extra/destructuring.js
@@ -0,0 +1,8 @@
+const numerosPares = [2, 4, 6]
+const numerosImpares = [1, 3, 5]
+
+//quero juntar os dois arrays acima em um:
+
+const numeros = [...numerosPares, ...numerosImpares]
+console.log(numeros); // saída: [ 2, 4, 6, 1, 3, 5 ]
+
